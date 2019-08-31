b724759a54dc913db1bd098f2a5d58fdfacbffff
Tomas Echeverri Valencia <tomechval@gmail.com>
2019-08-31 16:05:18 +0200

    Move express app to its own `server.js` module

--- src/index.js
+++ src/index.js
@@ -1,43 +1,5 @@
-import { ApolloServer } from "apollo-server-express"
-import bodyParser from "body-parser"
-import cors from "cors"
-import express from "express"
-import HttpStatus from "http-status-codes"
-import jsonwebtoken from "jsonwebtoken"
-import uuid from "uuid/v4"
-import { CORS_ORIGIN_HEADER, JWT_SECRET_KEY, PORT } from "./config"
-import dataSources from "./datasources"
-import { ensureValidToken, rateLimiter } from "./middlewares"
-import resolvers from "./resolvers"
-import typeDefs from "./schema"
-
-const app = express()
-
-var corsOptions = {
-  origin: CORS_ORIGIN_HEADER,
-}
-
-app.use(cors(corsOptions))
-
-app.use(bodyParser.json())
-
-app.post("/login", function loginHandler(req, res) {
-  const payload = { userId: uuid() }
-  const options = { jwtid: uuid() }
-  const token = jsonwebtoken.sign(payload, JWT_SECRET_KEY, options)
-
-  return res.status(HttpStatus.OK).json({ token: token })
-})
-
-app.use("/graphql", ensureValidToken, rateLimiter)
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  dataSources,
-})
-
-server.applyMiddleware({ app })
+import { PORT } from "./config"
+import app from "./server"
 
 app.listen(PORT, () => {
   console.log(`🚀Server ready on port ${PORT}`)
